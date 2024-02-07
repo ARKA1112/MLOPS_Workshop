cbc61ff0651917edf647c530906f4780fa95465c
ARKA1112 <bagha92@gmail.com>
2024-02-07 13:11:00 +0530

    ..

---
+++ 4_deployment/web service/predict.py
@@ -0,0 +1,17 @@
+import pickle
+import sklearn
+
+with open("extra_trees_reg.bin",'rb') as f_in:
+    dv,model = pickle.load(f_in)
+
+
+def prepare_data(ride):
+    features = {}
+    features["PU_DO"] = '%s_%s' % (ride['PULocationID'] , ride['DOLocationID'])
+    features['trip_distance'] = ride['trip_distance']
+    return features
+
+def predict(features):
+    X = dv.transform(features)
+    preds = model.predict(X)
+    return preds
